d75b017d2f794c73e89bdf8c525938b3997c0191
Hugi <hugiasgeirsson@gmail.com>
2016-02-23 23:47:11 +0100

    Update fastqc.sh

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -15,5 +15,5 @@ input=/proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/Sample_SN10_U
 #####
 
 # mkdir /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/fastqc_results
- fastqc -o /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/fastqc_results /proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/Sample_SN10_UNST/SN10_UNST_ATCACG_L001_R1_001.fastq.gz
+fastqc -o /home/hugia/proj5_ASE/BB2490-RNASeq-Project/results/2016-02-23/fastq_results_trimgalore_SN10_UNST_ATCACG_L001_R2 /home/hugia/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/SN10_UNST_ATCACG_L001_R1_001_val_1.fq.gz
 #fastqc -o /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/fastqc_results /proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/Sample_SN10_UNST/SN10_UNST_ATCACG_L001_R2_001.fastq.gz
