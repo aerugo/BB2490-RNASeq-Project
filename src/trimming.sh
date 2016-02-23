c5da045177bb99775a51ce3afdbc9d9053c87d54
Hugi Asgeirsson <hugia@milou2.uppmax.uu.se>
2016-02-23 15:50:15 +0100

    trimming script

--- src/trimming.sh
+++ src/trimming.sh
@@ -4,10 +4,10 @@
 #SBATCH -J Trim_test                                                                                                                                                                                                                         
 #SBATCH -p node -n 1                                                                                                                                                                                                                          
 #SBATCH -C thin                                                                                                                                                                                                                               
-#SBATCH -e /proj/g2015056/nobackup/info/trim_err.txt                                                                                                                                                                      
-#SBATCH -o /proj/b2012056/nobackup/info/trim_out.txt                                                                                                                                                                      
+#SBATCH -e /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/trim_err.txt                                                                                                                                                                      
+#SBATCH -o /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/trim_out.txt                                                                                                                                                                      
 #SBATCH --mail-type=All                                                                                                                                                                                                                       
-#SBATCH --mail-user=sailendra.pradhananga@scilifelab.se                                                                                                                                                                                      
+#SBATCH --mail-user=hugia@kth.se                                                                                                                                                                                      
 
 module add bioinfo-tools
 module add python/2.7
@@ -15,17 +15,19 @@ module load FastQC
 module load cutadapt
 module load TrimGalore
 
+output=/proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/
+input=/proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/Sample_SN10_UNST/
 
 #####To check: Better to use those?                                                                                                                                                                                                           
 ## nextera1=CTGTCTCTTATACACATCTGACGCTGCCGACGA                                                                                                                                                                                                 
 ## nextera2=CTGTCTCTTATACACATCTCCGAGCCCACGAGAC                                                                                                                                                                                                
 #####                                                                                                                                                                                                                                         
 
-nextera=CTGTCTCTTATACACATCT
+adapter=ATCACG
 
 ##Nextera                                                                                                                                                                                                                                     
-trim_galore -a ${nextera} -a2 ${nextera} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0143_1.fastq.gz ${input}S0143_2.fastq.gz &
-trim_galore -a ${nextera} -a2 ${nextera} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0156_1.fastq.gz ${input}S0156_2.fastq.gz &
-trim_galore -a ${nextera} -a2 ${nextera} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0160_1.fastq.gz ${input}S0160_2.fastq.gz &
+trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 1 --paired --length 25 -o ${output} ${input}SN10_UNST_ATCACG_L001_R1_001.fastq.gz ${input}SN10_UNST_ATCACG_L001_R1_002.fastq.gz
+##trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0156_1.fastq.gz ${input}S0156_2.fastq.gz &
+##trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0160_1.fastq.gz ${input}S0160_2.fastq.gz &
 wait
 
