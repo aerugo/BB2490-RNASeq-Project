d6de5626d2cfcdcba2e0c0954fc9f2c4279a406e
Hugi Asgeirsson <hugia@milou1.uppmax.uu.se>
2016-02-23 23:40:47 +0100

    nothing

--- src/trimming.sh
+++ src/trimming.sh
@@ -9,12 +9,13 @@
 #SBATCH --mail-type=All                                                                                                                                                                                                                       
 #SBATCH --mail-user=hugia@kth.se                                                                                                                                                                                      
 
-module add bioinfo-tools
+module load bioinfo-tools
 module add python/2.7
-module load FastQC
-module load cutadapt
-module load TrimGalore
+module add FastQC
+module add cutadapt
+module add TrimGalore
 
+mkdir -p /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/
 output=/proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/
 input=/proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/Sample_SN10_UNST/
 
@@ -26,8 +27,8 @@ input=/proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/Sample_SN10_U
 adapter=ATCACG
 
 ##Nextera                                                                                                                                                                                                                                     
-trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 1 --paired --length 25 -o ${output} ${input}SN10_UNST_ATCACG_L001_R1_001.fastq.gz ${input}SN10_UNST_ATCACG_L001_R1_002.fastq.gz
-##trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0156_1.fastq.gz ${input}S0156_2.fastq.gz &
-##trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0160_1.fastq.gz ${input}S0160_2.fastq.gz &
-wait
+trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 1 --paired --length 25 -o ${output} ${input}SN10_UNST_ATCACG_L001_R1_001.fastq.gz ${input}SN10_UNST_ATCACG_L001_R2_001.fastq.gz
+#trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0156_1.fastq.gz ${input}S0156_2.fastq.gz &
+#trim_galore -a ${adapter} -a2 ${adapter} -q 25 --stringency 5 --paired --length 25 -o ${output} ${input}S0160_1.fastq.gz ${input}S0160_2.fastq.gz &
+#wait
 
