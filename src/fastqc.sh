44edfc4de07d67579ba46d32bb8921563834085c
Hugi Asgeirsson <hugia@milou2.uppmax.uu.se>
2016-02-25 14:08:19 +0100

    analysis without specified adapers

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -14,6 +14,6 @@ input=/proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/Sample_SN10_U
 ## nextera2=CTGTCTCTTATACACATCTCCGAGCCCACGAGAC
 #####
 
-# mkdir /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/fastqc_results
-fastqc -o /home/hugia/proj5_ASE/BB2490-RNASeq-Project/results/2016-02-23/fastq_results_trimgalore_SN10_UNST_ATCACG_L001_R2 /home/hugia/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/SN10_UNST_ATCACG_L001_R1_001_val_1.fq.gz
+mkdir -p /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/2016-02-25/fastq_results_trimgalore_SN10_UNST_ATCACG_L001_R2
+fastqc -o /home/hugia/proj5_ASE/BB2490-RNASeq-Project/results/2016-02-25/fastq_results_trimgalore_SN10_UNST_ATCACG_L001_R2 /home/hugia/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/20160225-trimgalore/SN10_UNST_ATCACG_L001_R1_001_val_1.fq.gz
 #fastqc -o /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/fastqc_results /proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/Sample_SN10_UNST/SN10_UNST_ATCACG_L001_R2_001.fastq.gz
