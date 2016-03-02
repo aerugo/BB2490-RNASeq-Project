ac882afb1f38c5b5e3217bb59a9baf1f9d27333a
aerugo <4uglyLIONS>
2016-03-02 17:27:06 +0100

    star mapping script

--- src/combine_flowcells.sh
+++ src/combine_flowcells.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2015056                                                                                                                                                                                     
 #SBATCH -t 4:00:00                                                                                                                                                                                                                           
 #SBATCH -J trim and combine
-#SBATCH -p node -n 8
+#SBATCH -p core -n 8
 #SBATCH -C thin                                                                                                                                                                                                                              
 #SBATCH -e /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/combine_flowcells_err.txt
 #SBATCH -o /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/combine_flowcells_out.txt
@@ -23,9 +23,6 @@ output=/proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_r
 input_fc1=/proj/g2015056/BB2490/proj5_ASE/130104_SN866_0197_AC1DLVACXX/
 input_fc2=/proj/g2015056/BB2490/proj5_ASE/130104_SN866_0198_BC1DAYACXX/
 
-## Combine with cat
-# Sample_SN_10_LPS
-
 cd ${output}
 
 for directory in ${input_fc1}/S*; do
@@ -33,10 +30,7 @@ for directory in ${input_fc1}/S*; do
     for file in *.fastq.gz; do
         file1=$(basename "$file")
         file2=${input_fc2}$(basename "$directory")/$(basename "$file")
-        echo "Start cat of "${file1}
         cat ${file1} ${file2} > ${output}${file1}
-        echo "Finished cat of "${output}${file1}
         gunzip ${output}${file1}
-        echo "Unzipped "${output}${file1}
     done
 done
