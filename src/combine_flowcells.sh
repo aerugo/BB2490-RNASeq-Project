62311f2ad848a895e88c83b6dd56223fdd203c1d
aerugo <4uglyLIONS>
2016-03-02 17:28:01 +0100

    star mapping script

--- src/combine_flowcells.sh
+++ src/combine_flowcells.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -l                                                                                                                                                                                                                              
 #SBATCH -A g2015056                                                                                                                                                                                     
-#SBATCH -t 4:00:00                                                                                                                                                                                                                           
+#SBATCH -t 10:00:00
 #SBATCH -J trim and combine
-#SBATCH -p core -n 8
+#SBATCH -p core -n 2
 #SBATCH -C thin                                                                                                                                                                                                                              
 #SBATCH -e /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/combine_flowcells_err.txt
 #SBATCH -o /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/combine_flowcells_out.txt
