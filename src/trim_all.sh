837626b6534b3ab40ed1d7121b6b8dc9171bde82
Hugi Asgeirsson <hugia@milou2.uppmax.uu.se>
2016-03-03 20:49:09 +0100

    changes by sailendra

--- src/trim_all.sh
+++ src/trim_all.sh
@@ -16,15 +16,19 @@ input=/proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_re
 output=/proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/20160302/trimmed/
 
 cd ${input}
-for file in *.fastq.gz; do
-    echo "first file"
-    filename=${basename ${file}}
-    substring=${filename:0:22}
-    echo ${substring}"R1_001_val_1.fastq"
-    ls ${substring}"R1_001_val_1.fastq"
-    #trim_galore -q 25 --stringency 1 --paired --length 25 -o ${output} ${read1} ${read2}
+
+for file in *.fastq; do
+    filename=$(basename "$file")
+    echo $filename
 done
 
+
+#for file in SN10*;
+#do
+    #echo "$file"
+    #echo "firstfile"
+#trim_galore -q 25 --stringency 1 --paired --length 25 --fastqc -o ${output} ${read1} ${read2}
+#done
 
 
 #module load bioinfo-tools FastQC
