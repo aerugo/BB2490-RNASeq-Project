9843f89c732653806c27f00b338cdd74b8770960
aerugo <4uglyLIONS>
2016-03-03 21:41:09 +0100

    trimming script

---
+++ src/trimmingscript.sh
@@ -0,0 +1,41 @@
+#! /bin/bash -l
+#SBATCH -A g2015056
+#SBATCH -t 4:00:00
+#SBATCH -J trim and combine
+#SBATCH -p node -n 8
+#SBATCH -C thin
+#SBATCH -e /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/trim_all_err.txt
+#SBATCH -o /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/trim_all_out.txt
+#SBATCH --mail-type=All
+#SBATCH --mail-user=sailendra.pradhananga@scilifelab.se
+
+
+## Trimming
+
+input=/proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/20160302/combined/
+output=/proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/data/preprocessed_reads/20160303/trimmed/
+
+cd ${input}
+for file in *.fastq.gz; do
+    echo "first file"
+    filename=${file##*}
+    substring=${filename:0:22}
+    paired_read1=${substring}"R1_001.fastq"
+    paired_read2=${substring}"R2_001.fastq"
+    trim_galore -q 25 --stringency 1 --paired --length 25 --fastqc -o ${output} ${paired_read1} ${paired_read2}
+done
+
+
+
+#module load bioinfo-tools FastQC
+#fastqc ${output}SN_10_LPS_CGATGT_L001_R1_001_val_1.fq.gz &
+#fastqc ${output}SN_10_LPS_CGATGT_L001_R2_001_val_2.fq.gz &
+#wait
+
+#fastqc ${output}SN10_UNST_ATCACG_L001_R1_001_val_1.fq.gz &
+#fastqc ${output}SN10_UNST_ATCACG_L001_R2_001_val_2.fq.gz &
+#wait
+#
+#mkdir -p /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/2016-02-26/fastq_results/
+#mv -v ${output}*.zip /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/2016-02-26/fastq_results/
+#mv -v ${output}*.html /proj/g2015056/BB2490/proj5_ASE/BB2490-RNASeq-Project/results/2016-02-26/fastq_results/
